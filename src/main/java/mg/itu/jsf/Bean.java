3a15be84bb77bc6fb400334d91aaf8b7a16fda45
ASUS <ASUS@DESKTOP-9TF6LVH>
2023-03-31 20:44:42 +0300

    commit de l'exercice 1 du tp2

---
+++ src/main/java/mg/itu/jsf/Bean.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.jsf;
+
+import jakarta.inject.Named;
+import jakarta.enterprise.context.Dependent;
+import jakarta.enterprise.context.RequestScoped;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author admin
+ */
+@Named(value = "bean")
+@RequestScoped
+public class Bean {
+
+    /**
+     * Creates a new instance of Bean
+     */
+    private int nombre;
+
+    public int getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(int nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Integer> getNombresSuivants() {
+        int nb = 5;
+        List<Integer> l = new ArrayList<>(nb);
+        for (int i = nombre; i < nombre + nb; i++) {
+            l.add(i);
+        }
+        return l;
+    }
+
+    
+
+    public Bean() {
+    }
+
+}
